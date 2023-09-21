7e471c3abb6dbaaf330ba49f2921912cefc5f542
RasulSz <sebziyevresul3@gmail.com>
2023-09-22 00:17:01 +0400

    ijatejs

--- index.js
+++
@@ -1,6 +0,0 @@
-const ClearBtn = document.querySelector(".btn-clear");
-
-export function addClearButtonListener(result,clearBtn){
-    clearBtn.addClearButtonListener("click",() =>{result.value = 0;});
-}
-
